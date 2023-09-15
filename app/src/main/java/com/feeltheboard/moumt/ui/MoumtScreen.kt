9c78c1ee0eadf60b5908590be02a487d09a57f77
rafaover <rafamsilva@gmail.com>
2023-09-15 11:01:44 +1000

    Created MoumtScreen.kt - Main app screen

---
+++ app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
@@ -0,0 +1,10 @@
+package com.feeltheboard.moumt.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.feeltheboard.moumt.ui.components.WaterCounter
+
+@Composable
+fun MoumtScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
