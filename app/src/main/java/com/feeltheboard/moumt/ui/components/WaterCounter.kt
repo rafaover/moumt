3bd7b3aa62f2a522244e29546ff9bf98b510d167
rafaover <rafamsilva@gmail.com>
2023-09-15 11:12:53 +1000

    Updated WaterCounter - Added composables to organize glasscounter and button to increment. - Added basic logic to the button

--- app/src/main/java/com/feeltheboard/moumt/ui/components/WaterCounter.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/components/WaterCounter.kt
@@ -1,18 +1,30 @@
 package com.feeltheboard.moumt.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.feeltheboard.moumt.R
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 2
-    Text(
-        text = stringResource(R.string.glasses_count, count),
-        modifier = Modifier.padding(dimensionResource(R.dimen.default_padding))
-    )
+    var count = 0
+    Column(
+        modifier = modifier.padding(dimensionResource(R.dimen.medium_padding))
+    ) {
+        Text(
+            text = stringResource(R.string.glasses_count, count),
+        )
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.small_padding))
+        ) {
+            Text(stringResource(R.string.drink_one_more))
+        }
+    }
 }
