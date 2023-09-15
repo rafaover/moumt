2984e9e3bf450f9a63ad019bd0b46dead84243c5
rafaover <rafamsilva@gmail.com>
2023-09-15 12:09:58 +1000

    Added TaskItem composable call on WaterCounter.

--- app/src/main/java/com/feeltheboard/moumt/ui/components/WaterCounter.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/components/WaterCounter.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,18 +14,31 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.feeltheboard.moumt.R
 
+/**
+ * Composable to increase and show amount of glasses of water and Tasks
+ */
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count by remember { mutableStateOf(0) }
     Column(
         modifier = modifier.padding(dimensionResource(R.dimen.medium_padding))
     ) {
-        Text(
-            text = stringResource(R.string.glasses_count, count),
-        )
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            Text(text = stringResource(R.string.glasses_count, count))
+
+            if (showTask) {
+                TaskItem(
+                    taskName = "Stand-up and Walk",
+                    onClose = { /*TODO*/ }
+                )
+            }
+        }
         Button(
             onClick = { count++ },
-            modifier = Modifier.padding(top = dimensionResource(R.dimen.small_padding))
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.small_padding)),
+            enabled = count < 10
         ) {
             Text(stringResource(R.string.drink_one_more))
         }
