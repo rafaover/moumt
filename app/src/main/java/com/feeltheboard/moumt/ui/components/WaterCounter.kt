4af07e5bde9c299ae0c9a959733690962fa642a1
rafaover <rafamsilva@gmail.com>
2023-09-15 11:38:37 +1000

    Added state to WaterCounter.kt - updated count variable to hold state. - Updated gradle with dependencies

--- app/src/main/java/com/feeltheboard/moumt/ui/components/WaterCounter.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/components/WaterCounter.kt
@@ -5,15 +5,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.feeltheboard.moumt.R
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+    var count by remember { mutableStateOf(0) }
     Column(
         modifier = modifier.padding(dimensionResource(R.dimen.medium_padding))
     ) {
