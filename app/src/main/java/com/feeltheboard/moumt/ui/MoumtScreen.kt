5136e9b4df3a1660f0e07adc6e22400ff2a67c05
rafaover <rafamsilva@gmail.com>
2023-09-15 14:37:06 +1000

    Changed count to rememberSaveable to maintain state - added minor comments

--- app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.feeltheboard.moumt.ui.components.WaterCounter
 
+/** main screen composable */
 @Composable
 fun MoumtScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier)
--- app/src/main/java/com/feeltheboard/moumt/ui/components/WaterCounter.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/components/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.feeltheboard.moumt.ui.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -8,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -22,25 +24,33 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(dimensionResource(R.dimen.medium_padding))
     ) {
-        var count by remember { mutableStateOf(0) }
-
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
+//            var showTask by remember { mutableStateOf(true) }
             Text(text = stringResource(R.string.glasses_count, count))
 
-            if (showTask) {
-                TaskItem(
-                    taskName = "Stand-up and Walk",
-                    onClose = { showTask = false }
-                )
-            }
+//            if (showTask) {
+//                TaskItem(
+//                    taskName = "Stand-up and Walk",
+//                    onClose = { showTask = false }
+//                )
+//            }
         }
-        Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = dimensionResource(R.dimen.small_padding)),
-            enabled = count < 10
+        Row(
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.small_padding))
         ) {
-            Text(stringResource(R.string.drink_one_more))
+            Button(
+                onClick = { count++ },
+                modifier = Modifier.padding(end = dimensionResource(R.dimen.small_padding)),
+                enabled = count < 10
+            ) {
+                Text(stringResource(R.string.drink_one_more))
+            }
+//            Button(
+//                onClick = { count = 0 },
+//            ) {
+//                Text(stringResource(R.string.clear))
+//            }
         }
     }
 }
