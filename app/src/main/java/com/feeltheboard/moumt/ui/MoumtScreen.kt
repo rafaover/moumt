32aafa78210af6978f953164cbec4fc251552168
rafaover <rafamsilva@gmail.com>
2023-09-17 13:13:07 +1000

    updated imports

--- app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
@@ -3,7 +3,6 @@ package com.feeltheboard.moumt.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.feeltheboard.moumt.ui.components.StatefulWaterCounter
-import com.feeltheboard.moumt.ui.components.WaterCounter
 
 /** main screen composable */
 @Composable
