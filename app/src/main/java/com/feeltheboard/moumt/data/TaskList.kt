b201b2800f9cf2c1e99b922e81b509fedf60fc2a
rafaover <rafamsilva@gmail.com>
2023-09-18 11:20:33 +1000

    Task List Generator: - refact private method to generate list - Created TaskList method using Lazycolumn to list items from generateTaskList

--- app/src/main/java/com/feeltheboard/moumt/data/TaskList.kt
+++ app/src/main/java/com/feeltheboard/moumt/data/TaskList.kt
@@ -1,8 +1,28 @@
 package com.feeltheboard.moumt.data
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.feeltheboard.moumt.ui.components.StatefulTaskItem
+
 /** Method to generate list of random tasks */
-private fun taskList() {
+private fun generateTaskList() =
     List(30) {
         Task(it, "Task # $it")
+    }
+
+@Composable
+fun TaskList(
+    modifier: Modifier = Modifier,
+    list: List<Task> = remember { generateTaskList() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) {task ->
+            StatefulTaskItem(taskName = task.label)
+        }
     }
 }
