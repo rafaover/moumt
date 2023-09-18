1a98b9892350057a81e5ca0c77599344cfd57aca
rafaover <rafamsilva@gmail.com>
2023-09-18 13:30:12 +1000

    Unfinishedf test files.

---
+++ app/src/test/java/com/feeltheboard/moumt/ComponentsTesting.kt
@@ -0,0 +1,17 @@
+package com.feeltheboard.moumt
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.feeltheboard.moumt.ui.components.StatelessWaterCounter
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MoumtUnitTesting {
+    @Test
+    fun statelessWaterCounter_CounterIsIncreasing_CounterUpdated() {
+    }
+}
