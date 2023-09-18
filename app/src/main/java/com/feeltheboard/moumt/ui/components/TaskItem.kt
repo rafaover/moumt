ac991b06c1626efa9f414039be1e8b4c37786f0d
rafaover <rafamsilva@gmail.com>
2023-09-18 10:31:18 +1000

    Updated TaskItem component. - Added a checkbox and parameters related to.

--- app/src/main/java/com/feeltheboard/moumt/ui/components/TaskItem.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/components/TaskItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -18,6 +19,8 @@ import com.feeltheboard.moumt.R
 @Composable
 fun TaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -30,6 +33,10 @@ fun TaskItem(
                 .weight(1f)
                 .padding(start = dimensionResource(R.dimen.medium_padding)),
             text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
         )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = stringResource(R.string.close))
