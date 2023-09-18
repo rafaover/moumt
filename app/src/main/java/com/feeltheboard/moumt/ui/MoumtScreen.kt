326faa5172a4c8c16a7460c6bf3039982b5ce597
rafaover <rafamsilva@gmail.com>
2023-09-18 13:15:43 +1000

    MoumtScreen.kt Updates - Updated MoumtScreen TaskList method call to receive the OnCloseTask argument to removeTask when closing a task.

--- app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
@@ -2,15 +2,23 @@ package com.feeltheboard.moumt.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import com.feeltheboard.moumt.data.ListGenerator
 import com.feeltheboard.moumt.data.TaskList
 import com.feeltheboard.moumt.ui.components.StatefulWaterCounter
 
 /** main screen composable */
 @Composable
 fun MoumtScreen(modifier: Modifier = Modifier) {
+    var ListGenerator = ListGenerator()
     Column(modifier = modifier) {
         StatefulWaterCounter()
-        TaskList()
+        var taskList = remember {
+            ListGenerator.generateTaskList().toMutableStateList()
+        }
+        TaskList(modifier, taskList, onCloseTask = { task -> taskList.remove(task)})
     }
 }
+
