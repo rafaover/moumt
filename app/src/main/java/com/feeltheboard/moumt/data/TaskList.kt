a38b3681c7752c3ac9a23d9410c877953c196d3a
rafaover <rafamsilva@gmail.com>
2023-09-18 11:44:05 +1000

    TaskList.kt: - Updated method to add a rememberSaveable to the list parameter. - created a var of state to remembreLazyListState. The composable function rememberLazyListState creates an initial state for the list using rememberSaveable. When the Activity is recreated, the scroll state is maintained without you having to code anything.

--- app/src/main/java/com/feeltheboard/moumt/data/TaskList.kt
+++ app/src/main/java/com/feeltheboard/moumt/data/TaskList.kt
@@ -4,20 +4,25 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import com.feeltheboard.moumt.ui.components.StatefulTaskItem
 
 /** Method to generate list of random tasks */
 private fun generateTaskList() =
-    List(30) {
+    List(20) {
         Task(it, "Task # $it")
     }
 
 @Composable
 fun TaskList(
     modifier: Modifier = Modifier,
-    list: List<Task> = generateTaskList()
+    list: List<Task> = rememberSaveable { generateTaskList() }
 ) {
+    /* The composable function rememberLazyListState creates an initial state for the list using
+    rememberSaveable. When the Activity is recreated, the scroll state is maintained without you
+     having to code anything.
+     */
     val lazyListState = rememberLazyListState()
     LazyColumn(
         modifier = modifier,
