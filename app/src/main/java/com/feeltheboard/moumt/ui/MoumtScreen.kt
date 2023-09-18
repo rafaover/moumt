4bd853a2e71ef902cb81d9134ac930d87aa0ac92
rafaover <rafamsilva@gmail.com>
2023-09-18 13:29:57 +1000

    MoumtScreen.kt: Updates - Created a TaskViewModel instance - removed unnecessary variables as it is on Viewmodel to be called. - Updated TaskList() List and OncloseTask to use viewmodel instance. - To remove task() using remove() method from viewmodel as well.

--- app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
@@ -2,23 +2,24 @@ package com.feeltheboard.moumt.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-import com.feeltheboard.moumt.data.ListGenerator
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.feeltheboard.moumt.data.TaskList
 import com.feeltheboard.moumt.ui.components.StatefulWaterCounter
 
 /** main screen composable */
 @Composable
-fun MoumtScreen(modifier: Modifier = Modifier) {
-    var ListGenerator = ListGenerator()
+fun MoumtScreen(
+    modifier: Modifier = Modifier,
+    taskViewModel: TaskViewModel = viewModel()
+) {
     Column(modifier = modifier) {
         StatefulWaterCounter()
-        var taskList = remember {
-            ListGenerator.generateTaskList().toMutableStateList()
-        }
-        TaskList(modifier, taskList, onCloseTask = { task -> taskList.remove(task)})
+        TaskList(
+            modifier = modifier,
+            list = taskViewModel.tasks,
+            onCloseTask = { task -> taskViewModel.remove(task) }
+        )
     }
 }
 
