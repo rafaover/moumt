fbd1db7841d6fe7f635d7078fbf2d33c0f09da09
rafaover <rafamsilva@gmail.com>
2023-09-18 11:21:00 +1000

    Called TaskList(), and inserte both calls inside a column.

--- app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
+++ app/src/main/java/com/feeltheboard/moumt/ui/MoumtScreen.kt
@@ -1,11 +1,16 @@
 package com.feeltheboard.moumt.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.feeltheboard.moumt.data.TaskList
 import com.feeltheboard.moumt.ui.components.StatefulWaterCounter
 
 /** main screen composable */
 @Composable
 fun MoumtScreen(modifier: Modifier = Modifier) {
-    StatefulWaterCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulWaterCounter()
+        TaskList()
+    }
 }
